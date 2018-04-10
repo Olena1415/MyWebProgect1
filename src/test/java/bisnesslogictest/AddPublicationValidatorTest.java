af262d2329667704e6cddef23af56b7f66dd6ea6
Olena1415 <nvelena1415@gmail.com>
2018-04-10 11:26:27 +0300

    add tests

---
+++ src/test/java/bisnesslogictest/AddPublicationValidatorTest.java
@@ -0,0 +1,42 @@
+package bisnesslogictest;
+import bisnesslogic.addpublication.*;
+import database.*;
+import bisnesslogic.Error;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+
+
+
+
+public class AddPublicationValidatorTest {
+
+    private PublicationDataBase database;
+    private AddPublicationValidator validator;
+
+    @Before
+
+    public void init() {
+        database = Mockito.mock(PublicationDataBase.class);
+        validator = new AddPublicationValidator(database);
+
+    }
+
+    @Test
+    public void shouldReturnErrorWhenTitleIsNull() {
+        List<Error> errors = validator.validate(null, "desc");
+        assertEquals(errors.size(), 1);
+        assertEquals(errors.get(0).getField(), "title");
+        assertEquals(errors.get(0).getErrorMessage(), "Must not be empty");
+    }
+
+
+
+    // дописать тест с проверкой description  и копией title
+}
